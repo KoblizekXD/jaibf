61e674d43d268c4bf7e17077fd786c4ee8832556
KoblizekXD <janprokupek@centrum.cz>
2025-02-20 19:44:52 +0100

    Create sample config & onEnable handler

--- src/main/java/org/jaibf/plugin/JAIBFPlugin.java
+++ src/main/java/org/jaibf/plugin/JAIBFPlugin.java
@@ -3,4 +3,8 @@ package org.jaibf.plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class JAIBFPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+    }
 }
